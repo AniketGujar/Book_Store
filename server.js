e29bc64adf1c1d8f3e68d21b4b2465e0cd94ff62
AniketGujar <gujaraniket7875@gmail.com>
2021-03-19 23:23:31 +0530

    Modified: Server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/bookstore'));
+app.use(express.static('dist/bookstore'));
 
 app.get('/*', (req,res)=>{
     res.sendFile('index.html',{root:'dist/bookstore/'})
